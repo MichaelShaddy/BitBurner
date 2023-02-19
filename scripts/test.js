6e111e0a20449988b05078c79b3cb1b23eb7db66
MichaelShadd <michaelshaddy@hotmail.com>
2023-02-19 00:05:11 -0800

    updated some things regarding my loops... having troubles with them. check back in later to fix issues.

--- scripts/test.js
+++ scripts/test.js
@@ -9,39 +9,27 @@ export async function main(ns) {
       const serverHackingLevel = ns.getServerRequiredHackingLevel(hostName)
       const serverMoney = ns.getServerMoneyAvailable(hostName)
       const serverSecurityLevel = ns.getServerSecurityLevel(hostName)
-      const serverMaxSecurityLevel = ns.getServerMinSecurityLevel(hostName)
+      const serverMinSecurityLevel = ns.getServerMinSecurityLevel(hostName)
 
 
-    while (ns.hasRootAccess(hostName) && serverHackingLevel <= playerHackingLevel && serverSecurityLevel > serverMaxSecurityLevel) {
-      tprint("Attempting to weaken:", hostName)
-      const weakenResults = await ns.weaken(hostName)
-      tprint("Weaken results:", weakenResults)
+    while (ns.hasRootAccess(hostName) && serverHackingLevel <= playerHackingLevel) {
+      if (serverSecurityLevel > serverMinSecurityLevel) {
+        tprint("Attempting to weaken: ", hostName)
+        const weakenResults = await ns.weaken(hostName)
+        tprint("Weaken results: ", weakenResults)
+      }else if (serverMoney < 5000 && !(serverMoney >= 5000000)) {
+          tprint("Attempting to grow: ", hostName)
+          const growResults = await ns.grow(hostName)
+          tprint("Grow results: ", growResults)
+      }else {
+        tprint("Attemping to hack: ", hostName)
+        const hackResults = await ns.hack(hostName);
+        tprint("hack results: ", hackResults)
+      }
     } 
-
-    while (ns.hasRootAccess(hostName) && serverHackingLevel <= playerHackingLevel && serverSecurityLevel <= serverMaxSecurityLevel && serverMoney < 5000 && !(serverMoney >= 50000)) {
-      tprint("Attempting to grow:", hostName)
-      const growResults = await ns.grow(hostName)
-      tprint("Grow results:", growResults)
-    }
-
-    while (ns.hasRootAccess(hostName) && serverHackingLevel <= playerHackingLevel && serverSecurityLevel <= serverMaxSecurityLevel) {
-      tprint("Attemping to hack:", hostName)
-      const hackResults = await ns.hack(hostName);
-      tprint("hack results:", hackResults)
-    }
   }
 }
 
 
 
 
-// has root access : CHECK
-// knows server hacking level : CHECK
-//
-//
-//
-//
-//
-//
-//
-//
