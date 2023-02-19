4e52385d7ce2fe1ba14346ef52a3234dba4af1ad
MichaelShadd <michaelshaddy@hotmail.com>
2023-02-18 23:17:24 -0800

    Added While loops to my code.

--- scripts/test.js
+++ scripts/test.js
@@ -7,16 +7,41 @@ export async function main(ns) {
       const hostName = neighbor[i]
       const playerHackingLevel = ns.getHackingLevel()
       const serverHackingLevel = ns.getServerRequiredHackingLevel(hostName)
+      const serverMoney = ns.getServerMoneyAvailable(hostName)
+      const serverSecurityLevel = ns.getServerSecurityLevel(hostName)
+      const serverMaxSecurityLevel = ns.getServerMinSecurityLevel(hostName)
 
-      if (ns.hasRootAccess(hostName) && serverHackingLevel <= playerHackingLevel) {
-        tprint("Attemping to hack:", hostName)
-        const hackResults = await ns.hack(hostName);
-        tprint("hack results:", hackResults)
-      } else {
-        tprint(`Cannot Hack Server: ${hostName} :(${serverHackingLevel})`)
-      }
 
+    while (ns.hasRootAccess(hostName) && serverHackingLevel <= playerHackingLevel && serverSecurityLevel > serverMaxSecurityLevel) {
+      tprint("Attempting to weaken:", hostName)
+      const weakenResults = await ns.weaken(hostName)
+      tprint("Weaken results:", weakenResults)
+    } 
+
+    while (ns.hasRootAccess(hostName) && serverHackingLevel <= playerHackingLevel && serverSecurityLevel <= serverMaxSecurityLevel && serverMoney < 5000 && !(serverMoney >= 50000)) {
+      tprint("Attempting to grow:", hostName)
+      const growResults = await ns.grow(hostName)
+      tprint("Grow results:", growResults)
     }
 
+    while (ns.hasRootAccess(hostName) && serverHackingLevel <= playerHackingLevel && serverSecurityLevel <= serverMaxSecurityLevel) {
+      tprint("Attemping to hack:", hostName)
+      const hackResults = await ns.hack(hostName);
+      tprint("hack results:", hackResults)
+    }
+  }
 }
 
+
+
+
+// has root access : CHECK
+// knows server hacking level : CHECK
+//
+//
+//
+//
+//
+//
+//
+//
